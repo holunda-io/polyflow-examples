f49bc0703176ba63f408cc1f4e338b37118b2374
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-11-27 17:18:48 +0100

    upgrade versions

--- scenarios/distributed-axon-server-local-polyflow/process-application-local-polyflow/src/main/kotlin/io/holunda/polyflow/example/process/approval/TestApplicationRunner.kt
+++ scenarios/distributed-axon-server-local-polyflow/process-application-local-polyflow/src/main/kotlin/io/holunda/polyflow/example/process/approval/TestApplicationRunner.kt
@@ -1,5 +1,10 @@
 package io.holunda.polyflow.example.process.approval
 
+import io.holunda.camunda.bpm.data.CamundaBpmData.builder
+import io.holunda.camunda.bpm.data.CamundaBpmData.stringVariable
+import io.holunda.camunda.taskpool.api.business.AuthorizationChange
+import io.holunda.camunda.taskpool.api.business.ProcessingType
+import io.holunda.polyflow.datapool.sender.DataEntryCommandSender
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
@@ -8,9 +13,24 @@ import java.util.*
 
 @Component
 class TestApplicationRunner(
-  val commandGateway: CommandGateway
+  val commandGateway: CommandGateway,
+  val dataEntryCommandSender: DataEntryCommandSender
 ) : ApplicationRunner {
+
+  companion object {
+    val VAR = stringVariable("var")
+  }
+
   override fun run(args: ApplicationArguments) {
     commandGateway.sendAndWait<Void>(TestAggregateCreateCommand(UUID.randomUUID().toString()))
+
+    dataEntryCommandSender.sendDataEntryChange(
+      entryId = UUID.randomUUID().toString(),
+      entryType = "test.DataEntry",
+      payload = builder().set(VAR, "value").build(),
+      type = "Test Data Entry",
+      state = ProcessingType.IN_PROGRESS.of("processing"),
+      authorizationChanges = listOf(AuthorizationChange.addUser("kermit"))
+    )
   }
 }
