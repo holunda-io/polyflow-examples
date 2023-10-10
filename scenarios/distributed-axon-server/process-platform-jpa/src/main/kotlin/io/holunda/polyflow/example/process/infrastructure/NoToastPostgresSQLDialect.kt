da557bfbc6a3b82de547ff3e8aa296a2344574f2
Patrick Schalk <patrick.schalk@holisticon.de>
2023-10-10 18:04:28 +0200

    Fixed distributed axon scenario (add sequence per table + mapping for bytea dialect + set hibernate defaults for preferred_instant_jdbc_type and preferred_uuid_jdbc_type)  - WIP

--- scenarios/distributed-axon-server/process-platform-jpa/src/main/kotlin/io/holunda/polyflow/example/process/infrastructure/NoToastPostgresSQLDialect.kt
+++ scenarios/distributed-axon-server/process-platform-jpa/src/main/kotlin/io/holunda/polyflow/example/process/infrastructure/NoToastPostgresSQLDialect.kt
@@ -3,6 +3,8 @@ package io.holunda.polyflow.example.process.infrastructure
 import org.hibernate.boot.model.TypeContributions
 import org.hibernate.dialect.PostgreSQLDialect
 import org.hibernate.service.ServiceRegistry
+import org.hibernate.type.SqlTypes
+import org.hibernate.type.descriptor.jdbc.BinaryJdbcType
 import org.hibernate.type.descriptor.sql.internal.DdlTypeImpl
 import java.sql.Types
 
@@ -14,5 +16,26 @@ class NoToastPostgresSQLDialect : PostgreSQLDialect() {
     ddlTypeRegistry.addDescriptor(DdlTypeImpl(Types.BLOB, "bytea", this))
     ddlTypeRegistry.addDescriptor(DdlTypeImpl(Types.CLOB, "bytea", this))
     ddlTypeRegistry.addDescriptor(DdlTypeImpl(Types.NCLOB, "bytea", this))
+  }
+
+  override fun columnType(sqlTypeCode: Int): String {
+    return when (sqlTypeCode) {
+      SqlTypes.BLOB -> "bytea"
+      else -> super.columnType(sqlTypeCode)
+    }
+  }
+
+  override fun castType(sqlTypeCode: Int): String {
+    return when (sqlTypeCode) {
+      SqlTypes.BLOB -> "bytea"
+      else -> super.castType(sqlTypeCode)
+    }
+  }
+
+  override fun contributeTypes(typeContributions: TypeContributions, serviceRegistry: ServiceRegistry) {
+    super.contributeTypes(typeContributions, serviceRegistry)
+    val jdbcTypeRegistry = typeContributions.typeConfiguration
+      .jdbcTypeRegistry
+    jdbcTypeRegistry.addDescriptor(Types.BLOB, BinaryJdbcType.INSTANCE)
   }
 }
