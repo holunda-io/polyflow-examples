ea01095b6f2c4684f3f27ede95bfef4076f89995
Lukas Taake <lukas.taake@holisticon.de>
2025-09-04 10:51:11 +0200

    chore(process-forms): bootstrap using standalone apis

--- components/approval/process-forms/src/main.ts
+++ components/approval/process-forms/src/main.ts
@@ -1,13 +1,30 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {environment} from 'environments/environment';
+import {EnvironmentHelperService} from 'app/services/environment.helper.service';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {ApiModule} from 'process/api.module';
+import {AppRoutingModule} from 'app/app-routing.module';
+import {AppComponent} from 'app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(
+          BrowserModule,
+          // generated server API
+          ApiModule.forRoot({ rootUrl: '/example-process-approval/rest' }),
+          // routing
+          AppRoutingModule
+        ),
+        EnvironmentHelperService,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
 
